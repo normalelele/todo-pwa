c90f8f5469bb051114f660a49c23e8d23ea6a790
normalelele <86640392+normalelele@users.noreply.github.com>
2026-05-09 11:11:01 -0500

    Fix formatting of delete icon path in App.js

--- src/App.js
+++ src/App.js
@@ -242,7 +242,7 @@ export default function App() {
                   <button style={{ ...styles.iconBtn, color: '#f87171' }} onClick={() => deleteTask(task.id)}>
                     <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                       <polyline points="3 6 5 6 21 6" />
-                      <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+                      <path d="M19  6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
                       <path d="M10 11v6M14 11v6" />
                       <path d="M9 6V4h6v2" />
                     </svg>
